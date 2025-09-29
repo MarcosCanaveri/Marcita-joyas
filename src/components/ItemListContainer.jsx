9b1fbeeccd5c94e339144f7a186b2c80a3a29483
MarcosCanaveri <marcoscanaveri@gmail.com>
2025-09-29 20:16:05 -0300

    products

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,26 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 export function ItemListContainer() {
+
+    const [products, setProducts] = useState([])
+    console.log(products)
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const resultado = await fetch("https://dummyjson.com/products")
+                const resultado2 = await resultado.json()
+                setProducts(resultado2.products)
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
+
+        getData()
+    }, [])
+
+
     //acciones    
 
     const [count, setCount] = useState(0)
@@ -20,6 +39,10 @@ export function ItemListContainer() {
 
             <section className="main__section">
                 <h2 className="main__title">Soy el item list container</h2>
+                {products.map((products) => {
+                    return <p>{products.title}</p>
+                })}
+
 
                 <div className="products">
 
