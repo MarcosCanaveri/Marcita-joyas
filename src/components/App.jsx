962210f984e73681448ae10bdd90387efc1529d9
MarcosCanaveri <marcoscanaveri@gmail.com>
2025-10-14 22:56:45 -0300

    confirm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,32 +4,15 @@ import { Routes, Route } from "react-router-dom"
 import { ItemDetailContainer } from "./ItemDetailContainer"
 import { Footer } from "./Footer"
 import { Cart } from "./Cart"
-import { db } from "../firebaseConfig"
-import { addDoc, collection } from "firebase/firestore"
-import { Button } from "antd"
 
 
 
 
 function App() {
 
-  const handlePrueba = () => {
-
-    const productoCollection = collection(db, "productos")
-
-    const consulta = addDoc(productoCollection,{
-      id: 1,
-      nombre: "producto",
-      precio: 100,
-    })
-
-  }
-
-
   return (
     <>
       <Navbar />
-      <Button onClick={handlePrueba}>prueba</Button>
       <Routes>
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/productos" element={<p>Soy el producto</p>} />
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,30 @@
-export function Cart () {
+import { db } from "../firebaseConfig"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 
-    const handleConfirm = () => {
+export function Cart() {
 
-        
+    const handlePrueba = () => {
+
+        const nueva_orden = {
+            usuario : {
+                nombre : "Diego",
+                email : "diego@gmail.com"
+            },
+            fecha_compra : serverTimestamp(),
+            items : []
+        }
+
+        const ordenesCollection = collection(db, "ordenes")
+
+        const consulta = addDoc(ordenesCollection, nueva_orden)
+
     }
 
-    return  (
+    return (
         <main className="main">
             <section className="main__section"></section>
             <h1 className="main__title">Carrito de compras</h1>
-            <button onClick={handleConfirm}>Confirm</button>
+            <button onClick={handlePrueba}>Confirm</button>
         </main>
     )
 }
